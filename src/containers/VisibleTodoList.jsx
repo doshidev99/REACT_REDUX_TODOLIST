6c029f5be9832487f4f999653d90e57ef6c8e45f
SHYN <shyn@SHYNs-MacBook-Pro.local>
2019-08-01 18:28:24 +0700

    update

--- src/containers/VisibleTodoList.jsx
+++ src/containers/VisibleTodoList.jsx
@@ -5,23 +5,11 @@ import {
   editTodoAction,
 } from '../redux/todos/actions';
 import TodoList from '../components/TodoList';
-
-const getVisibleTodos = (todos, filter) => {
-  switch (filter) {
-    case 'SHOW_ALL':
-      return todos;
-    case 'SHOW_COMPLETED':
-      return todos.filter(t => t.completed);
-    case 'SHOW_ACTIVE':
-      return todos.filter(t => !t.completed);
-    default:
-      throw new Error(`Unknown filter: ${filter}`);
-  }
-};
+import getVisibleTodos from '../redux/filter/selectos';
 
 export default connect(
   state => ({
-    todos: getVisibleTodos(state.todos, state.visibilityFilter),
+    todos: getVisibleTodos(state),
   }),
   dispatch => ({
     toggleTodo: id => dispatch(toggleTodoAction(id)),
